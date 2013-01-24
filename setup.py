1962e136563fc403e410a27bb7e4c5340235e24a
Richard Graham <richardg@sjsoft.com>
2013-01-24 12:01:06 +0200

    Update dependencies and dependency_links.

--- setup.py
+++ setup.py
@@ -20,13 +20,13 @@ setup(
         'Django==1.4.2',
         'django-tagging==0.3.1',
         'sentry>=5.0.21',
-        'graphite-web>=0.9.10',
+        'graphite-web==0.9.10',
         'CherryPy>=3.2.2',
         'sentry_jsonmailprocessor',
         ],
     dependency_links=[
-        'http://github.com/Supy/sentry-smtpforwarder/tarball/master#egg=sentry_smtpforwarder-0.0.1',
-        'http://github.com/Supy/graphite-web/tarball/0.9.x-win#graphite-web-0.9.10'
+        'http://github.com/Supy/sentry_jsonmailprocessor/tarball/master#egg=sentry_jsonmailprocessor-0.0.1',
+        'http://github.com/richg/graphite-web/tarball/0.9.x#egg=graphite-web-0.9.10'
         ],
     **setup_kwargs
 )
