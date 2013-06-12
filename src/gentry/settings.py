0de3759d4c1c6d7810661058721cf69fbd8ef622
matth <matth@sjsoft.com>
2013-06-12 10:16:17 +0200

    Make a template gentry_settings.py file.

--- src/gentry/settings.py
+++ src/gentry/settings.py
@@ -31,7 +31,7 @@ def get_databases(warden_home):
     }
 
 # Make this unique, and don't share it with anybody.
-SENTRY_KEY = None
+#SENTRY_KEY = e.g. 'Pn9/XwYOZhj/AmWN1tNvqG6D+/NBNHdasZAg+jb1xTD4SA3yZL1I7A=='
 
 # Set this to false to require authentication
 SENTRY_PUBLIC = True
